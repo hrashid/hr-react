86fe8c90ebb66e547b9fc2c4da4f69c83fd5a89a
Harris Rashid <hrashid@pentaho.com>
2018-02-27 16:06:36 -0800

    update package json file to use react scripts and not webpack, deploying build.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import "./styles/app.scss";
+//import "./styles/app.scss";
 import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
